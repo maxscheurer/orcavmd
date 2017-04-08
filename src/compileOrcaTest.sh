3509bb516c7b22256f209d76bff223d7dfe52e49
Max Scheurer <max.scheurer@me.com>
2017-04-08 23:33:50 +0200

    comment in compile file

--- src/compileOrcaTest.sh
+++ src/compileOrcaTest.sh
@@ -13,3 +13,5 @@ fi
 fi
 gcc main.c orcaplugin.c -I../../include -o plugintest -lm
 ./plugintest orca $filepath
+
+#make LINUXAMD64 -j9; sudo cp compile/lib_LINUXAMD64/molfile/orcaplugin.so /usr/local/lib/vmd/plugins/LINUXAMD64/molfile/
