e577cfcf75f882e64d6c0e25bf4eeb7a7dff85e7
Max Scheurer <max.scheurer@me.com>
2017-04-10 15:25:57 +0200

    editing compileOrcaTest.sh to run on every system

--- src/compileOrcaTest.sh
+++ src/compileOrcaTest.sh
@@ -1,16 +1,17 @@
-filepath=/home/max/Projects/OrcaVMD/water.out
+filepath=$(pwd)/orca_testfiles/water.out
 if [ "$1" ]; then
 
 if [ $1 == "f" ]; then
 	echo "reading fake input"
-	filepath=/home/max/Projects/OrcaVMD/fake.out
+	filepath=$(pwd)/orca_testfiles/fake.out
 elif [ $1 == "s" ]; then
 	echo "reading wrong version file"
-	filepath=/home/max/Projects/OrcaVMD/unsupp.out
+	filepath=$(pwd)/orca_testfiles/unsupp.out
 else
 	filepath=$1
 fi
 fi
+echo $filepath
 gcc main.c orcaplugin.c -I../../include -o plugintest -lm
 ./plugintest orca $filepath
 
